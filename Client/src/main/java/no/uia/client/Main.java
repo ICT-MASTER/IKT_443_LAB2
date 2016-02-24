ec85e58d40c1821df125dfdf3a16eba008ac3096
Per-Arne Andersen <perara12@gmail.com>
2016-02-24 16:54:43 +0100

    Initial Commit

---
+++ Client/src/main/java/no/uia/client/Main.java
@@ -0,0 +1,22 @@
+package no.uia.client;
+
+/**
+ * Created by per on 24.02.16.
+ */
+public class Main {
+
+
+
+    public static void main(String [] args){
+
+
+        Client c = new Client("localhost", 25000);
+        c.start();
+
+
+    }
+
+
+
+
+}
---
+++ Server/src/main/java/no/uia/server/Main.java
@@ -0,0 +1,16 @@
+package no.uia.server;
+
+/**
+ * Created by per on 24.02.16.
+ */
+public class Main {
+
+
+    public static void main(String [] args){
+
+        Server srv = new Server();
+        srv.start();
+
+
+    }
+}
